fe1981b3f5334251d21a9d805b97310d4a97787f
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-23 16:52:22 +0200

    config: add the not-found component for react-router-dom

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+// import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div>
+      NotFound
+      <Link to={"/"}>Retourner sur la page d'accueil</Link>
+    </div>
+  );
+};
+
+export default NotFound;
