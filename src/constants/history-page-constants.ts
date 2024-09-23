8943a61369b02f399ed64bb7b22244ec239242c7
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-23 09:47:37 +0200

    config: add the html elements text for the history page

---
+++ src/constants/history-page-constants.ts
@@ -0,0 +1,29 @@
+// Types
+
+const historyIntroduction = {
+  historyTitle: "Conakry est la capitale et la plus grande ville de Guinée.",
+  historyIntro:
+    "Conakry est bien plus qu'une simple ville. Située à la pointe de l’Afrique de l’Ouest, entre l’océan Atlantique et un arrière-pays riche en histoire, Conakry est un carrefour où tradition et modernité se rencontrent. Au fil des siècles, cette cité insulaire est devenue le cœur battant de la culture guinéenne, un lieu de créativité effervescente, où les rythmes des percussions traditionnelles côtoient les sons électroniques des musiques contemporaines.Foyer d'une population diverse, Conakry est aussi le reflet des dynamiques sociales et économiques de la Guinée. Entre ses marchés colorés, où les étals regorgent d’artisanat et de produits locaux, et ses quartiers modernes en pleine expansion, la ville incarne l’esprit d’une nation en mouvement. En racontant l’histoire de Conakry, on plonge dans un récit où les identités se tissent, où le passé colonial dialogue avec un futur prometteur.Parcourons ensemble les étapes de cette métamorphose fascinante, de ses humbles débuts en tant que village côtier à son statut actuel de capitale culturelle et politique de l’Afrique de l’Ouest. ",
+};
+
+const etymology = {
+  etymologyTitle: "Étymologie",
+  etymologyDescription:
+    "Selon une légende, à l’origine, il y avait dans l’île Tombo, non loin de l’actuel port, un framager géant sous lequel un paysan baga du nom de Cona avait construit sa case. Sa palmeraie produisait le meilleur  vin de palme  de l’île ; les gens de Kaporo venaient donc boire sous le fromager de Cona. Ils disaient alors : « Je vais chez Cona, sur l’autre rive (nakiri) ». Ainsi, par contraction, le lieu devint Conakry.",
+};
+
+const situation = {
+  situationTitle: "Situation",
+  situationDescription:
+    "Située au sud-ouest de la Guinée, sur la presqu'île de Camayenne, près des îles de Loos, l'agglomération de Conakry s'étend sur la plaine côtière parcourue de petits fleuves qui descendent du Fouta-Djalon. Le territoire de la ville est orienté au nord-est/sud-ouest et se termine par la péninsule de Kaloum et l'île de Tombo.Tout comme Dakar, Conakry est une ville-péninsule. La presqu'île de Camayenne se présente comme un promontoire rocheux d'une altitude maximum de 146 mètres. Cernée à la fois par la mer et par des forêts de mangroves sa largeur ne dépasse pas les 6,5 kilomètres. Le sol y est en grande partie latéritique. Le mont Kakoulima, culminant à 1 011 mètres d'altitude, domine le Nord du paysage.",
+  // situationImg: ,
+  // situationCaption: ,
+};
+
+const history = {
+  historyTitle: "Histoire",
+  historyDescription:
+    "Le territoire où se trouve Conakry appartenait au royaume de Dubréka. La région est alors occupée par les Bagas, qui avaient accueilli des Soussous, venus du nord du Mandingue après la destruction de leur capitale sur le Niger en 1236 par Soundjata Keïta. En 1880, Balé Demba, manga (roi) de Dubréka, signe un traité de protectorat avec la France, point de départ de la colonisation de la Guinée. Le 1er février 1885, il cède aux Français un terrain sur l'île de Tombo, près du village de Conakry, En 1887, les Anglais reconnaissent que l'île de Tombo, abritant les quatre villages de Conakry, Boulbinet, Krutown et Tombo, relève des Français . Pendant la période française, Conakry devient la capitale de la colonie des « Rivières du Sud » en 1889, puis de la « Guinée française et dépendances », colonie autonome placée sous l’autorité du gouvernement général de Dakar. En 1890, l’île de Tombo ne compte encore que 150 habitants. Sélectionné par les colons français pour sa capacité à accueillir un port en eau profonde, le site a fait l'objet d’un régime de développement extraverti fondé sur l'exploitation des ressources naturelles, notamment minières. La bauxite fut ainsi exploitée dans le périmètre actuel de la ville de Conakry, puis dans des régions plus éloignées.De 1966 à 1972, l'ancien président ghanéen Kwame Nkrumah y vit en exil et y fonde une maison d'édition.",
+};
+
+export { historyIntroduction, etymology, situation, history };
