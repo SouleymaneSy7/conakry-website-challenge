698d3617540bcd743c25f7c7dd7c365263e8d7b4
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-23 13:06:10 +0200

    config: add the config for react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import History from "./pages/History";
+import Contacts from "./pages/Contacts";
+
 const App = () => {
-  return <div>Hello Conakry</div>;
+  return (
+    <div>
+      Hello Conakry
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
