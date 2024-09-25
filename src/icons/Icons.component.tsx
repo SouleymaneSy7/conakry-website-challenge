ecffa32448e0a0620d82da856af38b32c661d8d7
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-25 08:16:40 +0200

    feat: add the link arrow icons

--- src/icons/Icons.component.tsx
+++ src/icons/Icons.component.tsx
@@ -48,7 +48,13 @@ export const FooterLogo = (props: SVGProps<SVGSVGElement>) => {
 
 export const FacebookIcon = (props: SVGProps<SVGSVGElement>) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 320 512" {...props}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={32}
+      height={32}
+      viewBox="0 0 320 512"
+      {...props}
+    >
       <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
     </svg>
   );
@@ -106,6 +112,21 @@ export const YoutubeIcon = (props: SVGProps<SVGSVGElement>) => {
       {...props}
     >
       <path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z" />
+    </svg>
+  );
+};
+
+export const LinkArrowIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      viewBox="0 0 0.266559 0.380955"
+      fillRule="evenodd"
+      clipRule="evenodd"
+      {...props}
+    >
+      <g>
+        <polygon points="0.266559,0.13328 0.13328,0 -0,0.13328 0.0355433,0.168819 0.108154,0.0962126 0.10815,0.304764 0.158409,0.304752 0.158413,0.0962165 0.231016,0.168819 " />
+      </g>
     </svg>
   );
 };
