ad5af98b6bf779238c98319db391e5ff2f8436bc
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-25 19:29:39 +0200

    feat: change the html structure

--- src/components/HomeMainCommuneSection.tsx
+++ src/components/HomeMainCommuneSection.tsx
@@ -3,26 +3,28 @@ import { communeSection, communeSlider } from "../constants/homepage-constants";
 
 const HomeMainCommuneSection = () => {
   return (
-    <Container as={"section"} className="commune-section | container">
-      <div>
-        <h2>{communeSection.communeTitle}</h2>
-        <p>{communeSection.communeDescription}</p>
-      </div>
+    <Container as={"section"} className="commune-section">
+      <div className="container">
+        <div>
+          <h2>{communeSection.communeTitle}</h2>
+          <p>{communeSection.communeDescription}</p>
+        </div>
 
-      <div>
-        <Container as={"div"}>
-          {communeSlider.map((slider) => {
-            return (
-              <Container as={"article"} key={slider.id}>
-                <h3>{slider.sliderTitle}</h3>
-                <p>{slider.sliderDescription}</p>
-              </Container>
-            );
-          })}
-          {/* Buttons */}
-        </Container>
+        <div>
+          <Container as={"div"}>
+            {communeSlider.map((slider) => {
+              return (
+                <Container as={"article"} key={slider.id}>
+                  <h3>{slider.sliderTitle}</h3>
+                  <p>{slider.sliderDescription}</p>
+                </Container>
+              );
+            })}
+            {/* Buttons */}
+          </Container>
 
-        {/* <div className="commune-map"></div> */}
+          {/* <div className="commune-map"></div> */}
+        </div>
       </div>
     </Container>
   );
--- src/components/HomeMainExploreSection.tsx
+++ src/components/HomeMainExploreSection.tsx
@@ -1,41 +1,42 @@
 import { exploreCards, exploreSection } from "@/constants/homepage-constants";
 import Container from "./Container";
 import Button from "./Buttons";
-// import Button from "./Buttons";
 
 const HomeMainExploreSection = () => {
   return (
-    <Container as={"section"} className="explore-section | container">
-      <div className="explore__flex">
-        <div className="explore__textbox">
-          <h2>{exploreSection.explorerTitle}</h2>
-          <p>{exploreSection.explorerDescription}</p>
-        </div>
+    <Container as={"section"} className="explore-section">
+      <div className="container">
+        <div className="explore__flex">
+          <div className="explore__textbox">
+            <h2>{exploreSection.explorerTitle}</h2>
+            <p>{exploreSection.explorerDescription}</p>
+          </div>
 
-        {/* <div className="explore__arrows">
+          {/* <div className="explore__arrows">
           <Button type="button">arrow</Button>
         </div> */}
-      </div>
+        </div>
 
-      <Container as={"div"} className="explore-card-container">
-        {exploreCards.map((card) => {
-          return (
-            <Container as={"article"} key={card.id} className="explore-card">
-              {/* <div className="explore-card__img">
+        <Container as={"div"} className="explore-card-container">
+          {exploreCards.map((card) => {
+            return (
+              <Container as={"article"} key={card.id} className="explore-card">
+                {/* <div className="explore-card__img">
                 <img src={card.cardImg} alt="" />
               </div> */}
 
-              <h3 className="explore-card__title">{card.cardTitle}</h3>
-              <p className="explore-card__description">
-                {card.cardDescription}
-              </p>
-              <Button type="button" className="explore-card--btn">
-                {card.cardBtn}
-              </Button>
-            </Container>
-          );
-        })}
-      </Container>
+                <h3 className="explore-card__title">{card.cardTitle}</h3>
+                <p className="explore-card__description">
+                  {card.cardDescription}
+                </p>
+                <Button type="button" className="explore-card--btn">
+                  {card.cardBtn}
+                </Button>
+              </Container>
+            );
+          })}
+        </Container>
+      </div>
     </Container>
   );
 };
