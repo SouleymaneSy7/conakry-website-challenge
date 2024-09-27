fecca647b226720d981ac432272e776256f21786
Souleymane Sy <souleymanesyservices@gmail.com>
2024-09-27 20:37:10 +0200

    feat: add the commune contents slider

--- src/components/HomeMainCommuneSection.tsx
+++ src/components/HomeMainCommuneSection.tsx
@@ -1,30 +1,50 @@
+import React from "React";
 import Container from "./Container";
 import { communeSection, communeSlider } from "../constants/homepage-constants";
 import LeafletMapContainer from "./LeafletMapContainer";
+import Button from "./Buttons";
 
 const HomeMainCommuneSection = () => {
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  const prevHandler = () => {
+    setCurrentIndex((index) => {
+      return index === 0 ? communeSlider.length - 1 : index - 1;
+    });
+  };
+
+  const nextHandler = () => {
+    setCurrentIndex((index) => {
+      return index === communeSlider.length - 1 ? 0 : index + 1;
+    });
+  };
+
   return (
     <Container as={"section"} className="commune-section">
       <div className="container">
-        <div>
+        <div className="commune__textbox">
           <h2>{communeSection.communeTitle}</h2>
           <p>{communeSection.communeDescription}</p>
         </div>
 
-        <div>
-          <Container as={"div"}>
-            {communeSlider.map((slider) => {
-              return (
-                <Container as={"article"} key={slider.id}>
-                  <h3>{slider.sliderTitle}</h3>
-                  <p>{slider.sliderDescription}</p>
-                </Container>
-              );
-            })}
-            {/* Buttons */}
+        <div className="commune__flex">
+          <Container as={"div"} className="commune__card-container">
+            <Container as={"article"}>
+              <h3>{communeSlider[currentIndex].sliderTitle}</h3>
+              <p>{communeSlider[currentIndex].sliderDescription}</p>
+            </Container>
+
+            <div className="commune__card-btn-container">
+              <Button type="button" className="prev-btn" onClick={prevHandler}>
+                {"<"}
+              </Button>
+              <Button type="button" className="next-btn" onClick={nextHandler}>
+                {">"}
+              </Button>
+            </div>
           </Container>
 
-          <div className="commune-map">
+          <div className="commune__map">
             <LeafletMapContainer />
           </div>
         </div>
